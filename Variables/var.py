d0f6d62946852a50f21f73df9b52e65326523ced
siraajul <shirajulislamparvez@gmail.com>
2024-03-14 18:00:07 +0600

    --working with variable

---
+++ Variables/var.py
@@ -0,0 +1,11 @@
+# assigning variables
+name = "Parvez"
+print(name)
+# reassigning variables
+name = "Sirajul"
+print(name)
+
+first_name = "Sirajul"
+last_name = "Islam"
+# print multiple variable at once
+print(first_name,last_name)
