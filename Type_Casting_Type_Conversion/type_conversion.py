8ee5b212fa27deb0de89784bd3d4ad39a5c1287f
siraajul <shirajulislamparvez@gmail.com>
2024-03-22 16:57:56 +0600

    --working with type conversion

---
+++ Type_Casting_Type_Conversion/type_conversion.py
@@ -0,0 +1,21 @@
+# type conversion
+a = "12"
+print(type(a))
+
+# conversion from sting to integer
+a = int(a)
+print(a + 12)
+
+# conversion from string to float
+b = float(a)
+print(b)
+
+# conversion from int to string
+c = 123
+d = str(c)
+print(d + '123')
+
+# false values - False, None, Zero Numeric Values, Empty Sequence & Collections
+e = "1234"
+f = bool(e)
+print(f)
