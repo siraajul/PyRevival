12e6dd13a74c7671edc43ea23a8570764c905e41
siraajul <shirajulislamparvez@gmail.com>
2024-03-22 15:25:06 +0600

    --working with variables

--- Variables/var.py
+++ Variables/var.py
@@ -8,9 +8,12 @@ print(name)
 first_name = "Sirajul"
 last_name = "Islam"
 # print multiple variable at once
-print(first_name,last_name)
+print(first_name, last_name)
 
 # lets more simplify it
 
 school, address = 'Brahmanbaria High School', 'Brahmanbaria'
-print(school, address,sep=',')
+print(school, address, sep=',')
+
+mahesh = 12
+print(mahesh)
