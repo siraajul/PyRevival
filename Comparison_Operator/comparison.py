02744dc99b7e49c804f9b728ebf0e178d88ccee1
siraajul <shirajulislamparvez@gmail.com>
2024-03-23 06:22:47 +0600

    --working with various comparison operators

---
+++ Comparison_Operator/comparison.py
@@ -0,0 +1,22 @@
+a = 100
+b = 200
+
+# Comparison Always give answer in boolean
+
+# ' == ' compare booth side equals or not
+print(a == b)
+
+# '!=' not equal to both side are not same
+print(a != b)
+
+# ' > 'greater than
+print(a > b)
+
+# ' < ' less than
+print(a < b)
+
+# ' >= ' greater or equal
+print(a >= b)
+
+# ' <= ' less or equal
+print(a <= b)
