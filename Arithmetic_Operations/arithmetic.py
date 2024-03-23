bf555bf0f453fb6d49e024f72870ba56a0bb1e12
siraajul <shirajulislamparvez@gmail.com>
2024-03-23 06:22:34 +0600

    --working with various arithmetic operators

---
+++ Arithmetic_Operations/arithmetic.py
@@ -0,0 +1,12 @@
+# whenever we want to use mathematical operation we need to use arithmetic operator
+print(12 + 13)
+
+a = 12
+b = 23
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)  # single always give floating point number as an answer if it is integer
+print(a // b)  # flore division it will give answer non-floating
+print(a ** b)  # double astric on the left side it is number and on the right side it is its power like 12 power 23
+print(a % b)  # MOD - modules function
