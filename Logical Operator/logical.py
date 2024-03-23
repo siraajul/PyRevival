3079b8c6faedcb84f6f58e0ec4b6f93008d78fc4
siraajul <shirajulislamparvez@gmail.com>
2024-03-23 06:23:04 +0600

    --working with various logical operators

---
+++ Logical Operator/logical.py
@@ -0,0 +1,7 @@
+# AND - OR - NOT
+print(12 < 13 and 34 > 23)  # if 1 condition is false,it will give answer false. if both conditions are true, it is true
+print(12 < 13 or 34 > 23)  # if 1 condition it true it will give true
+print(not 12 < 13)  # not converts result opposite it answer is true it will give false, vice versa!
+print(not True)
+print(not False)
+print(not 12 > 13 and 34 > 12 and not 67 < 34)
