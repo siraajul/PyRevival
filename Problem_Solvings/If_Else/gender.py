7a271b56edfba7f07521a180c31fe4b4b58e9454
siraajul <shirajulislamparvez@gmail.com>
2024-03-23 16:23:29 +0600

    Question--Check Gender M or F (Conditioners)

---
+++ Problem_Solvings/If_Else/gender.py
@@ -0,0 +1,7 @@
+gender = str(input("Provide your gender: "))
+if gender == "m" or gender == "M":
+    print("Hello Sir")
+elif gender == "f" or gender == "F":
+    print("Hello Ma'am")
+else:
+    print("Are You Human ?")
