26eb530833fa37f1c641b6bc9d198141ca7ec370
siraajul <shirajulislamparvez@gmail.com>
2024-03-23 08:40:30 +0600

    --working with elif

---
+++ Control Flow/el_if.py
@@ -0,0 +1,7 @@
+item = input("which ice-cream do you have?")
+if item == "pista":
+    print("Please Pack Pista IceCream")
+elif item == "vanilla":
+    print("Please Pack Vanilla IceCream")
+else:
+    print("Dont Pack Anything")
