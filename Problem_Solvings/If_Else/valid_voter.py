0b8e6f0bd901f2c259eb0b26d66cfe51694a242e
siraajul <shirajulislamparvez@gmail.com>
2024-03-24 13:27:36 +0600

    Question--Check Valid Voter or Not

---
+++ Problem_Solvings/If_Else/valid_voter.py
@@ -0,0 +1,6 @@
+name = str(input("Enter your name: "))
+age = int(input("Enter your age: "))
+if age >= 18:
+    print(f"Congratulations {name}. You are a voter! ")
+else:
+    print(f"Sorry {name}. You are not a voter")
