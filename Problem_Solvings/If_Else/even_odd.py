4f28b944bae43f2a2cdb769e75efd92a62db633e
siraajul <shirajulislamparvez@gmail.com>
2024-03-25 07:39:12 +0600

    Question--Check Input Even or Odd

--- Problem_Solvings/If_Else/even_odd.py
+++ Problem_Solvings/If_Else/even_odd.py
@@ -1,5 +1,7 @@
 number = int(input('Enter a number: '))
 if number % 2 == 0:
     print(f"{number} is an even number")
-else:
+elif number % 2 == 1:
     print(f"{number} is an odd number")
+else:
+    print(f"This is Character not number")
